26656b32341f72e1a727a1b7a0ebf20b5af45dda
elaygl <elaygl@gmail.com>
2019-09-09 14:01:43 +0300

    refresh every 15 mins

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+setTimeout(() => {
+  window.location.reload();
+}, 1000 * 60 * 15);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
